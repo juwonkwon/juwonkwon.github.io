067c3f658c27e65171afbf2c2344b78207ae3543
Juwon Kwon <jk6240@nyu.edu>
2023-06-04 01:52:43 -0400

    made new project sections

--- app.js
+++ app.js
@@ -10,6 +10,76 @@ window.onscroll = function() {
     document.getElementById("navigation").style.top = "-5vw";
   }
   
-  previous= current;
+  previous = current;
   
 }
+var background_image = document.getElementById("preview");
+var social_one = document.getElementById('social1');
+var social_two = document.getElementById('social2');
+var social_three =  document.getElementById('social3');
+var social_four = document.getElementById('social4');
+
+
+document.getElementById("social1").onmouseover = function() {
+  social_one.setAttribute("style","padding-bottom:0vw;opacity:0.3;width: 22vw;height: 0vw;font-size: 3.3vw;"); 
+  background_image.style.backgroundImage = "url(image/insta.PNG)";
+  background_image.style.opacity = "0.5";
+  background_image.style.border = "thick solid #bc1888";
+  background_image.style.transition = "ease .1s"; }
+
+document.getElementById("social1").onmouseout  = function() { 
+  social_one.setAttribute("style","");
+  social_two.setAttribute("style","");
+  social_three.setAttribute("style","");
+  social_four.setAttribute("style","");
+  background_image.style.backgroundImage = "url(image/Capture.PNG)";
+  background_image.style.border = "none";
+  background_image.style.transition = "ease .1s";}
+
+document.getElementById("social2").onmouseover = function() { 
+  social_two.setAttribute("style","padding-bottom:0vw;opacity:0.3;width: 22vw;height:0vw;font-size: 3.3vw;");
+  background_image.style.backgroundImage = "url(image/li.PNG)"; 
+  background_image.style.opacity = "0.5";
+  background_image.style.border = "thick solid #007bb5";
+  background_image.style.transition = "ease .1s";}
+
+document.getElementById("social2").onmouseout  = function() { 
+  social_one.setAttribute("style","");
+  social_two.setAttribute("style","");
+  social_three.setAttribute("style","");
+  social_four.setAttribute("style","");
+  background_image.style.backgroundImage = "url(image/Capture.PNG)"; 
+  background_image.style.border = "none";
+  background_image.style.transition = "ease .1s";}
+
+document.getElementById("social3").onmouseover = function() { 
+  social_three.setAttribute("style","padding-bottom:0vw;opacity:0.3;width: 22vw;height: 0vw;font-size: 3.3vw;");
+  background_image.style.backgroundImage = "url(image/fb.PNG)";
+  background_image.style.opacity = "0.5";
+  background_image.style.transition = "ease .1s"; 
+  background_image.style.border = "thick solid #3B5998";}
+
+document.getElementById("social3").onmouseout  = function() { 
+  social_one.setAttribute("style","");
+  social_two.setAttribute("style","");
+  social_three.setAttribute("style","");
+  social_four.setAttribute("style","");
+  background_image.style.backgroundImage = "url(image/Capture.PNG)"; 
+  background_image.style.border = "none";
+  background_image.style.transition = "ease .1s";}
+
+document.getElementById("social4").onmouseover = function() { 
+  social_four.setAttribute("style","padding-bottom:0vw;opacity:0.3;width: 22vw;height: 0vw;font-size: 3.3vw;");
+  background_image.style.backgroundImage = "url(image/git.PNG)"; 
+  background_image.style.opacity = "0.5";
+  background_image.style.border = "thick solid #111111";
+  background_image.style.transition = "ease .1s";}
+  
+document.getElementById("social4").onmouseout  = function() { 
+  social_one.setAttribute("style","");
+  social_two.setAttribute("style","");
+  social_three.setAttribute("style","");
+  social_four.setAttribute("style","");
+  background_image.style.backgroundImage = "url(image/Capture.PNG)"; 
+  background_image.style.border = "none";
+  background_image.style.transition = "ease .1s";}
